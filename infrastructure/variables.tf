0aa781b11bf08a786fb8fb8cd3ac402926a33b38
p1sci5k3y <enricodedur@gmail.com>
2026-03-10 14:38:27 +0100

    feat(ops): Add Phase 9 AWS EC2 Terraform, Docker Compose, and CI/CD pipelines

---
+++ infrastructure/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS Region where the existing infrastructure resides"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "existing_vpc_id" {
+  description = "ID of the existing VPC"
+  type        = string
+  default     = "vpc-0626552ef2d408abe"
+}
+
+variable "existing_subnet_id" {
+  description = "ID of the existing Subnet"
+  type        = string
+  default     = "subnet-0e769394628914359"
+}
+
+variable "existing_instance_id" {
+  description = "ID of the existing EC2 instance"
+  type        = string
+  default     = "i-07b8b2ff00c18d875"
+}
