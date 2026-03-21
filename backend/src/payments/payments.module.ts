4ea893f65338b998ac9a2dd78b80cc53b3891588
p1sci5k3y <enricodedur@gmail.com>
2026-03-21 16:14:21 +0100

    refactor: extract StripeWebhookService from PaymentsService

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -5,11 +5,12 @@ import { OnboardingController } from './onboarding.controller';
 import { PaymentsController } from './payments.controller';
 import { OrdersModule } from '../orders/orders.module';
 import { PaymentsService } from './payments.service';
+import { StripeWebhookService } from './stripe-webhook.service';
 
 @Module({
   imports: [ConfigModule, OrdersModule],
   controllers: [WebhooksController, OnboardingController, PaymentsController],
-  providers: [PaymentsService],
+  providers: [PaymentsService, StripeWebhookService],
   exports: [PaymentsService],
 })
 export class PaymentsModule {}
