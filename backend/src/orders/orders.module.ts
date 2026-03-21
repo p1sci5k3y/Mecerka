cedf284fd65330c81acb01b3e6f729a0b45d13f0
p1sci5k3y <enricodedur@gmail.com>
2026-03-21 15:21:12 +0100

    refactor: extract OrderItemsService from OrdersService

--- backend/src/orders/orders.module.ts
+++ backend/src/orders/orders.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { OrderItemsService } from './order-items.service';
 import { OrdersController } from './orders.controller';
 import { GeocodingModule } from '../geocoding/geocoding.module';
 import { PrismaModule } from '../prisma/prisma.module';
@@ -8,7 +9,7 @@ import { RiskModule } from '../risk/risk.module';
 @Module({
   imports: [PrismaModule, RiskModule, GeocodingModule],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrderItemsService],
   exports: [OrdersService],
 })
 export class OrdersModule {}
