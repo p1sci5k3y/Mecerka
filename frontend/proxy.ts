40c81ccb5ac96e9cab087e455becba13118d8168
p1sci5k3y <enricodedur@gmail.com>
2026-03-25 20:43:09 +0100

    fix: exclude runtime-config from locale proxy

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -4,5 +4,5 @@ import { routing } from './lib/navigation';
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+  matcher: ['/((?!api|runtime-config|_next|_vercel|.*\\..*).*)'],
 };
